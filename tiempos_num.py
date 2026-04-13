eb02f5d9d7353ff221aefae65053a89eddc85cb6
carguello1275-vc <carguello1803@gmail.com>
2026-04-13 14:54:41 -0600

    Update tiempos_num.py

--- tiempos_num.py
+++ tiempos_num.py
@@ -17,62 +17,62 @@ def run_script_numbers():
 
     def run_script_check():
         try:
-        scraper = cloudscraper.create_scraper()
-        url = "https://integration.jps.go.cr/api/App/nuevostiempos/historical"
-        today = date.today()
+            scraper = cloudscraper.create_scraper()
+            url = "https://integration.jps.go.cr/api/App/nuevostiempos/historical"
+            today = date.today()
 
-        params = {
+            params = {
                    "fechaInicio": "2026-01-01",
                   "fechaFin": today
                     }
 
-        headers = {
+            headers = {
                     "Authorization": "sec_num"
                     }
 
-        response = scraper.get(url, params=params, headers=headers)
+            response = scraper.get(url, params=params, headers=headers)
 
-        data = response.json()
+            data = response.json()
 
 
-        rows = []
+            rows = []
 
-        for day in data:
-        dia = day.get("dia")
+            for day in data:
+            dia = day.get("dia")
 
-        for periodo in ["manana", "mediaTarde", "tarde"]:
-        draw = day.get(periodo)
+            for periodo in ["manana", "mediaTarde", "tarde"]:
+            draw = day.get(periodo)
 
-        if draw:
-            rows.append({
-                "dia": dia,
-                #"periodo": periodo,
-                "numero": draw.get("numero"),
-                #"meganNumero": draw.get("meganNumero??"),
-                #"premio": draw.get("premio"),
-                #"reventado": draw.get("in_reventado"),
-                #"numeroSorteo": draw.get("numeroSorteo"),
-                #"fecha_sorteo": draw.get("fecha"),
-                #"porcentaje": draw.get("porcentaje"),
-                #"color": draw.get("colorBolita")
+            if draw:
+                rows.append({
+                    "dia": dia,
+                    #"periodo": periodo,
+                    "numero": draw.get("numero"),
+                    #"meganNumero": draw.get("meganNumero??"),
+                    #"premio": draw.get("premio"),
+                    #"reventado": draw.get("in_reventado"),
+                    #"numeroSorteo": draw.get("numeroSorteo"),
+                    #"fecha_sorteo": draw.get("fecha"),
+                    #"porcentaje": draw.get("porcentaje"),
+                    #"color": draw.get("colorBolita")
+                })
+
+            df = pd.DataFrame(rows)
+
+            df["dia"] = pd.to_datetime(df["dia"]).dt.strftime("%d/%m/%Y")
+
+            output = io.StringIO()
+            df.to_csv(output, index=False)
+            csv_data = output.getvalue()
+
+            return Response(
+            csv_data,
+            mimetype="text/csv",
+            headers={"Content-Disposition": "attachment; filename=Numeros_favorecidos.csv"
             })
 
-        df = pd.DataFrame(rows)
-
-        df["dia"] = pd.to_datetime(df["dia"]).dt.strftime("%d/%m/%Y")
-
-        output = io.StringIO()
-        df.to_csv(output, index=False)
-        csv_data = output.getvalue()
-
-        return Response(
-        csv_data,
-        mimetype="text/csv",
-        headers={"Content-Disposition": "attachment; filename=Numeros_favorecidos.csv"
-        })
-
-    except Exception as e:
-    return jsonify({
-            "error": str(e),
-            "type": str(type(e))
-        })
+        except Exception as e:
+            return jsonify({
+               "error": str(e),
+                "type": str(type(e))
+            })
